301d82051eaabf6c61624eca2eb18ada7214ebbd
frozen531 <2332274884@qq.com>
2019-02-25 10:56:02 +0800

    QML调用C++类中的属性

--- gemini.h
+++ gemini.h
@@ -9,6 +9,13 @@ class Gemini : public QObject // 必须共有继承自QObject，这样才可以�
 {
     Q_OBJECT // 必须写，用来声明信号与槽
     Q_ENUMS(BALL_COLOR) // // 枚举类型，要想在QML中使用该枚举，就需要加宏Q_ENUMS()
+    /*
+     * Q_PROPERTY(类型 变量名 READ 读取变量函数名 WRITE 设置变量函数名) // 这里的变量名就是QML调用时用的名字
+     * READ：读取属性值，如果没有设置MEMBER的话，它是必需的。一般情况下，函数是个const函数，返回值类型必须是属性本身的类型或这个类型的const引用，没有参数。
+     * WRITE：设置属性值，可选项。函数必须返回void，有且仅有一个参数，参数类型必须是属性本身的类型或这个类型的指针或引用。
+     * NOTIFY：与属性关联的可选信号。这个信号必须在类中声明过，当属性值改变时，就可触发这个信号，可以没有参数，有参数的话只能是一个类型同属性本身类型的参数，用来记录属性改变后的值。
+    */
+    Q_PROPERTY(unsigned int ballNumber READ ballNumber WRITE setBallNumber NOTIFY ballNumberChanged)
 
 public:
     Gemini():m_ballColor(BALL_COLOR_YELLOW){ // 使用枚举默认的初始化列表
@@ -26,8 +33,21 @@ public:
         qDebug()<<"Gemini::stop() called";
     }
 
+    unsigned int ballNumber() const { // 读取属性值函数
+        return m_ballNumber;
+    }
+
+    void setBallNumber(unsigned int& ballNumber) { // 设置属性值函数
+        if(ballNumber != m_ballNumber) {
+            m_ballNumber = ballNumber;
+            emit ballNumberChanged(); // 信号在C++中使用时要用到emit关键字,而在QML中就是普通的函数
+        }
+    }
+
 signals:
     void begin();
+    void ballNumberChanged(); // Q_PROPERTY中NOTIFY的函数名，与属性关联的可选信号。这个信号必须在类中声明过，当属性值改变时，就可触发这个信号，
+                              // 可以没有参数，有参数的话只能是一个类型同属性本身类型的参数，用来记录属性改变后的值。
 
 public slots:
     void doSomething(BALL_COLOR ballColor) {
@@ -40,5 +60,6 @@ public slots:
 
 private:
     BALL_COLOR m_ballColor;
+    unsigned int m_ballNumber; // QML要访问的成员变量
 };
 #endif // GEMINI_H
--- main.qml
+++ main.qml
@@ -15,12 +15,16 @@ Window {
     Gemini{
         id:gemini
         onBegin: doSomething(Gemini.BALL_COLOR_RED) // begin信号函数，响应doSomething()槽函数，使用时是<用类名>.<枚举类型变量>
+        onBallNumberChanged: console.log("new ball number is", ballNumber) // Q_PROPERTY中与属性关联的信号函数
+        Component.onCompleted: console.log("default ball number is", ballNumber) // 如果属性没有变，执行
     }
+
     MouseArea{
         anchors.fill: parent
         onClicked: {
             gemini.begin() // 会先响应begin()对应的后续响应函数
             gemini.stop() // begin()对应的后续响应函数doSomething()响应完后才执行stop()
+            gemini.ballNumber = 10 // 属性赋值
         }
     }
 }
